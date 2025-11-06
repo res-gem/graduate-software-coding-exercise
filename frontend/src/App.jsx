a4441de106bb33624dca82b4980ba11dda55d7bb
Zain Siddiqi <zain.siddiqi@res-group.com>
2025-11-06 12:56:48 +0000

    init frontend directory

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // your code here
+  };
+
+  return (
+    <div style={{ 
+      fontFamily: 'sans-serif',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      minHeight: '100vh'
+    }}>
+      <h2>Emoji Translator 😎</h2>
+      {/* your code here */}
+    </div>
+  );
+}
+
+export default App;
